5f61592fcbd8b3e960df14cb62c0313b6d6f0e02
HyeongtaekOh <oh052679@gmail.com>
2024-01-27 05:12:03 +0900

    필요없는 코드 정리

--- src/main/java/com/example/websocketapp/service/AmqpService.java
+++
@@ -1,8 +0,0 @@
-package com.example.websocketapp.service;
-
-import com.example.websocketapp.domain.ChatMessage;
-
-public interface AmqpService {
-
-    public void sendMessage(String exchange, String routingKey, ChatMessage message);
-}
--- src/main/java/com/example/websocketapp/service/ChatService.java
+++ src/main/java/com/example/websocketapp/service/ChatService.java
@@ -2,7 +2,6 @@ package com.example.websocketapp.service;
 
 import com.example.websocketapp.domain.ChatMessage;
 import com.example.websocketapp.dto.ChatMessageDto;
-import com.example.websocketapp.repository.ChatHistoryRepository;
 import com.example.websocketapp.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final AmqpService amqpService;
-
     private final ChatRepository chatRepository;
-    private final ChatHistoryRepository chatHistoryRepository;
 
     public ChatMessage save(ChatMessageDto chatMessageDto) {
         ChatMessage chatMessage = ChatMessage.of(chatMessageDto);
