370945e03d989d5a150816245bb7954afc7a4367
HyeongtaekOh <oh052679@gmail.com>
2024-01-27 05:09:10 +0900

    no message broker version

--- src/main/java/com/example/websocketapp/controller/ChatController.java
+++ src/main/java/com/example/websocketapp/controller/ChatController.java
@@ -2,14 +2,13 @@ package com.example.websocketapp.controller;
 
 import com.example.websocketapp.domain.ChatMessage;
 import com.example.websocketapp.dto.ChatMessageDto;
-import com.example.websocketapp.service.AmqpService;
 import com.example.websocketapp.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,22 +21,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatController {
 
-    @Value("${rabbitmq.exchange.name}")
-    private String exchange;
-
-    @Value("${rabbitmq.queue.name}")
-    private String queue;
-
     private final ChatService chatService;
-    private final AmqpService amqpService;
 
     @MessageMapping("/chat/{roomId}")
-    public void greeting(@DestinationVariable(value = "roomId") String roomId, @Payload ChatMessageDto messageDto) {
+    @SendTo("/topic/messages/{roomId}")
+    public ChatMessage greeting(@DestinationVariable(value = "roomId") String roomId, @Payload ChatMessageDto messageDto) {
         log.info("roomId: {}, message: {}", roomId, messageDto);
-        String routingKey = queue + "." + messageDto.getRoomId();
-        log.info("routingKey: {}", routingKey);
-        ChatMessage message = chatService.save(messageDto);
-        amqpService.sendMessage(exchange, routingKey, message);
+        return chatService.save(messageDto);
     }
 
     @ResponseBody
